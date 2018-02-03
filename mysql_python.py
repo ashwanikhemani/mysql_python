9e63e2446a5a956ca300887494076aa2cb870026
Ashwani Khemani <ashwani.khemani@gmail.com>
2018-02-03 12:42:38 -0600

    Initial commit for fetching the data from mysql

---
+++ mysql_python.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Script in Python that goes over our user database and creates users on Intercom
+
+"""
+import MySQLdb
+from intercom.client import Client
+
+hostname="host"
+username="db_user"
+password="pwd"
+database_name="db_name"
+
+#Create mysqldb connection 
+myConn = MySQLdb.connect(host=hostname,user=username,passwd=password,db=database_name)
+cur=myConn.cursor();
+
+# Configuration of client and Authorization
+intercom = Client(personal_access_token='access_token')
+
+#Fetch list of users 
+cur.execute("select * from user")
+for id,user_name,user_email in cur.fetchall():
+    print "id:",id,"user name:",user_name,"user email:",user_email
+    user = intercom.users.create(user_id=id,email=user_email, name=user_name)
+
+# List Users
+for user in intercom.users.all():
+    print user
+    
+# Create or Update User  
+user = intercom.users.create(user_id=id,email=user_email, name=user_name)
+user = intercom.users.create(used_id=id, email='new_email@new.com')
+
+# delete a user
+user = intercom.users.find(user_id=id)
+deleted_user = intercom.users.delete(user)
+
+# Submit job to create users 
+intercom.users.submit_bulk_job(create_items=[{'user_id': 10, 'email': 'a@new.com'}, {'user_id': 11, 'email': 'b@new.com'}])
+
+# Submit job to delete users
+intercom.users.submit_bulk_job(delete_items=[{'user_id': 10, 'email': 'a@new.com'}, {'user_id': 11, 'email': 'b@new.com'}])
+
